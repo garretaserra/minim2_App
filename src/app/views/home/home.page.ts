a9810076f9ad614cceadd689fa3854f25233ccd1
sergi <sergi@unievegroup.com>
2019-12-19 12:24:41 +0100

    Finished selecting degree

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {Subject} from '../models/Subject';
-import {SubjectService} from '../services/subject.service';
+import {Subject} from '../../models/Subject';
+import {SubjectService} from '../../services/subject.service';
 import {Router} from '@angular/router';
 
 @Component({
@@ -19,12 +19,21 @@ export class HomePage {
     ) {}
 
     async ionViewWillEnter(){
+        this.updateSubjects();
+    }
+
+    async updateSubjects(){
         if(this.degree === 'all'){
             this.subjects = await this.subjectService.getSubjects().toPromise();
         }
         else{
             this.subjects = await this.subjectService.getSubjectsByDegree(this.degree).toPromise();
         }
+    }
+
+    async changeDegree(event){
+        this.degree = event.detail.value;
+        await this.updateSubjects();
     }
 
     async gotoSubject(subject){
