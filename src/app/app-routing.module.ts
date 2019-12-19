91f36a5fc16e0685547c3417bced7351cb2fe403
sergi <sergi@unievegroup.com>
2019-12-19 12:24:25 +0100

    Added student and subject pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./views/home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'subject',
+    loadChildren: () => import('./views/subject/subject.module').then( m => m.SubjectPageModule)
+  },
+  {
+    path: 'student',
+    loadChildren: () => import('./views/student/student.module').then( m => m.StudentPageModule)
+  },
 ];
 
 @NgModule({
