978d4b50ad76d145325f95d4fc5ecf00ae6509dd
sergi <sergi@unievegroup.com>
2019-12-20 12:59:09 +0100

    Create student with phones

--- src/app/views/create-student/create-student.page.ts
+++ src/app/views/create-student/create-student.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {StudentService} from '../../services/student.service';
+import {Student} from '../../models/Student';
 
 @Component({
   selector: 'app-create-student',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateStudentPage implements OnInit {
 
-  constructor() { }
+    student: Student = new Student();
 
-  ngOnInit() {
-  }
+    constructor(
+        private studentService: StudentService
+    ) {}
 
+    ngOnInit() {
+        this.student.phones = [{description: '', number: ''}];
+    }
+
+    async addStudent() {
+        await this.studentService.addNewStudent(this.student).toPromise();
+    }
+
+    async addPhone(){
+        this.student.phones.push({description: '', number: ''})
+    }
+
+    deleteSubject(phone) {
+        for(let i = 0; i < this.student.phones.length; i++){
+            if(this.student.phones[i] == phone){
+                this.student.phones.splice(i, 1);
+                return;
+            }
+        }
+    }
 }
