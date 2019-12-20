126155683152dc88a8797305eb3adf8bc1db1818
sergi <sergi@unievegroup.com>
2019-12-20 14:34:44 +0100

    Finished student dropping subject

--- src/app/views/subject/subject.page.ts
+++ src/app/views/subject/subject.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SubjectService} from '../../services/subject.service';
 import {Subject} from '../../models/Subject';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Student} from '../../models/Student';
 
 @Component({
   selector: 'app-subject',
@@ -11,6 +12,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class SubjectPage implements OnInit {
 
     subject: Subject;
+    nonEnrolledStudents: Student[]; //Students that are not enrolled in the subject
 
     constructor(
         private subjectService: SubjectService,
@@ -22,11 +24,28 @@ export class SubjectPage implements OnInit {
     }
 
     async ionViewWillEnter(){
-      let id = this.route.snapshot.paramMap.get('id');
-      this.subject = await this.subjectService.getSubjectFromId(id).toPromise();
+      this.updateStudents();
+    }
+
+    async updateStudents(){
+        let id = this.route.snapshot.paramMap.get('id');
+        this.subject = await this.subjectService.getSubjectFromId(id).toPromise();
+        this.nonEnrolledStudents = await this.subjectService.getStudentsNotEnrolled(this.subject.name).toPromise();
     }
 
     async gotoStudent(student){
         await this.router.navigateByUrl('/student/'+student._id);
+    }
+
+    async enrollStudents(event) {
+        await event.detail.value.forEach(async val=>{
+            await this.subjectService.enrollStudent(this.subject.name, val).toPromise();
+            await this.updateStudents();
+        });
+    }
+
+    async dropSubject(student){
+        await this.subjectService.dropSubject(this.subject.name, student.name).toPromise();
+        await this.updateStudents();
     }
 }
